1916292cc1de4ba80cb2a3cb64fe0b3db648f2b8
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 09:38:34 +0900

    chore: .gitignore 추가 (.build, .swiftpm, Package.resolved 제외)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "APSSPMediationNAM",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "APSSPMediationNAM", targets: ["APSSPMediationNAM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
+        .package(url: "https://github.com/naver/nam-sdk-ios.git", exact: "8.24.3"),
+    ],
+    targets: [
+        .target(name: "APSSPMediationNAM",
+                dependencies: [
+                    .product(name: "APSSPSDK", package: "ap-APSSPSDK-SPM"),
+                    .product(name: "GFPSDK", package: "nam-sdk-ios"),
+                ],
+                path: "Sources/MediationNAM"),
+    ]
+)

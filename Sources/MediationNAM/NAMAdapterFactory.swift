5b0febe0aeb2eee8b97652eb17aa291b0d1a4ac8
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 09:39:27 +0900

    8110600.0.0(8.11.6.0 Adapter)배포

--- Sources/MediationNAM/NAMAdapterFactory.swift
+++ Sources/MediationNAM/NAMAdapterFactory.swift
@@ -8,7 +8,7 @@ public final class NAMAdapterFactory: NSObject, APSSPAdapterFactory {
     public static var networkID: Int { 22 }
 
     public static var sdkVersion: String? { NAMInitializationAdapter().sdkVersion }
-    public static var adapterVersion: String? { "8.11.1.0" }
+    public static var adapterVersion: String? { "8.11.6.0" }
 
     public static func makeInitializationAdapter() -> AnyObject? {
         return NAMInitializationAdapter()

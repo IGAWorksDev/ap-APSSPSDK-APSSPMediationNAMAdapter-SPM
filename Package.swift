4d18b9dc1d1510319b17cb69c945349d3862f70d
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 09:39:39 +0900

    8120000.0.0(8.12.0.0 Adapter)배포

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
-        .package(url: "https://github.com/naver/nam-sdk-ios.git", exact: "8.11.6"),
+        .package(url: "https://github.com/naver/nam-sdk-ios.git", exact: "8.12.0"),
     ],
     targets: [
         .target(name: "APSSPMediationNAM",
